0fd077497532fb55caaa2ee82f57475b7c534ab3
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-09-25 16:02:22 -0400

    Initial Commit of SpringBoot

---
+++ src/main/java/com/gentilaj_Assignment0/GentilajAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.gentilaj_Assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentilajAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentilajAssignment0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gentilaj_Assignment0/GentilajAssignment0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gentilaj_Assignment0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentilajAssignment0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
